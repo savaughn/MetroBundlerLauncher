19f727dbd5ecba343af69e9a0caa100bed999aea
Spencer Vaughn <svaughn57@gmail.com>
2024-10-22 20:45:58 -0500

    Refactor code to disable/enable file entry in check_connection function

--- src/connection.c
+++ src/connection.c
@@ -36,6 +36,35 @@ int is_port_open(int port)
   return 1;
 }
 
+typedef enum {
+    WIDGETS_DISABLED,
+    WIDGETS_ENABLED
+} WidgetsState;
+
+static void set_widgets_state(Widgets *widgets, WidgetsState state)
+{
+    if (state == WIDGETS_DISABLED)
+    {
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->start_button), FALSE);    // Disable the start button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->terminate_button), TRUE); // Disable the terminate button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->restart_button), TRUE);   // Disable the restart button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->port_entry), FALSE);      // Disable the port entry
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->prefix_entry), FALSE);        // Disable the IP entry
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->hermes_checkbox), FALSE); // Disable the Hermes checkbox
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->file_entry), FALSE); // Disable the file entry
+    }
+    else
+    {
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->start_button), TRUE);      // Enable the start button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->terminate_button), FALSE); // Disable the terminate button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->restart_button), FALSE);   // Disable the restart button
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->port_entry), TRUE);        // Enable the port entry
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->prefix_entry), TRUE);          // Enable the IP entry
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->hermes_checkbox), TRUE);   // Enable the Hermes checkbox
+        gtk_widget_set_sensitive(GTK_WIDGET(widgets->file_entry), TRUE);   // Enable the file entry
+    }
+}
+
 // Function to periodically check the connection and update the label
 gboolean check_connection(gpointer data)
 {
@@ -51,25 +80,13 @@ gboolean check_connection(gpointer data)
     {
         draw_green_status_light(widgets);
         gtk_label_set_text(widgets->port_label, port_text);
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->start_button), FALSE);    // Disable the start button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->terminate_button), TRUE); // Enable the terminate button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->restart_button), TRUE);   // Enable the restart button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->port_entry), FALSE);      // Disable the port entry
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->prefix_entry), FALSE);        // Disable the IP entry
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->hermes_checkbox), FALSE); // Disable the Hermes checkbox
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->file_entry), FALSE); // Disable the file entry
+        set_widgets_state(widgets, WIDGETS_DISABLED);
     }
     else
     {
         draw_red_status_light(widgets);
         gtk_label_set_text(widgets->port_label, port_text);
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->start_button), TRUE);      // Enable the start button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->terminate_button), FALSE); // Disable the terminate button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->restart_button), FALSE);   // Disable the restart button
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->port_entry), TRUE);        // Enable the port entry
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->prefix_entry), TRUE);          // Enable the IP entry
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->hermes_checkbox), TRUE);   // Enable the Hermes checkbox
-        gtk_widget_set_sensitive(GTK_WIDGET(widgets->file_entry), TRUE);   // Enable the file entry
+        set_widgets_state(widgets, WIDGETS_ENABLED);
     }
 
     return TRUE; // Continue calling this function
